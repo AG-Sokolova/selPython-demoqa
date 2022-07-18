a0e66a7d01d4b2cf1bd69e4d0e39fcf3d8de6d68
qaSokolovaAG <nast1ageorg@gmail.com>
2022-07-18 11:02:56 +0400

    add test text box

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,10 +1,20 @@
+from pages.elements_page import TextBoxPage
 import time
-from pages.base_page import BasePage
-
 
 class TestElements:
 
-    def test(self, driver):
-        page = BasePage(driver, 'https://demoqa.com/text-box')
-        page.open()
-        time.sleep(5)
+    class TestTextBox:
+        def test_text_box(self, driver):
+            text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
+            text_box_page.open()
+
+            input_full_name, input_email, input_current_address, input_permanent_address = text_box_page.fill_all_fields()
+            output_full_name, output_email, output_current_address, output_permanent_address = text_box_page.check_filled_form()
+            assert input_full_name == output_full_name, "the full name does not match"
+            assert input_email == output_email, "the email does not match"
+            assert input_current_address == output_current_address, "the current address does not match"
+            assert input_permanent_address == output_permanent_address, "the permanent address does not match"
+
+            # input_data = text_box_page.fill_all_fields()
+            # output_data = text_box_page.check_filled_form()
+            # assert input_data == output_data
